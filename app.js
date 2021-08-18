ddf60509e98f1a649661564dc3f02a87f6c55c02
Kili90 <guido.h.ruiz@gmail.com>
2021-08-18 19:31:51 -0300

    FIrst Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { get } = require("http");
+const app = express();
+
+const path = require("path");
+
+app.listen(3000, () => {
+    console.log("Se prendió!");
+});
+
+//mETODOS DE HTTP
+// - GET
+// - POST
+// - PUT
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
